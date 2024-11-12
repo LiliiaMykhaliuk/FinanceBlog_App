e34c070a18c6c6552dcd68de501ff78cbfdd7303
Liliia <myhaliuklilia@gmail.com>
2024-11-12 19:33:32 +0100

    Change name of the allowed hosts (Beanstalk environment) to correct one

--- finance_blogapp/settings.py
+++ finance_blogapp/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = [
-    'financeblog.us-east-1.elasticbeanstalk.com',
+    'finance-blog.us-east-1.elasticbeanstalk.com',
     '127.0.0.1'
 ]
 
